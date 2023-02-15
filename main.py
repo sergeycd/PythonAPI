be48dcaf4f0258599093e098812929448d961c6c
Golovanov Sergey <golovanov911@gmail.com>
2023-02-15 21:21:58 +0400

    первый автотест апи

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import requests
 # вывод текста ответа
 response = requests.get("https://playground.learnqa.ru/api/hello")
 print(response.text)
+
+# тест из домашнего задания
+getText = requests.get("https://playground.learnqa.ru/api/get_text")
+print(getText.text)
